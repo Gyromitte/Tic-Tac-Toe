dadadc873ba67d99ccd6af7544fe999928cee156
Gyromitte <gyromitte@gmail.com>
2021-10-16 20:17:36 -0500

    Created the constructor for the gameflow

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-function gameBoard(Xmarks, Omarks){
-    this.Xmarks = Xmarks;
-    this.Omarks = Omarks;
-    
+function gameBoard(){
     let board = ["1",2,3,4,5,6,7,8,9];
 
     this.getGameboard = function(){
@@ -13,13 +10,33 @@ function gameBoard(Xmarks, Omarks){
     }
 }
 
+function gameFlow(firstPlayerChoices, secondPlayerChoices){
+    this.showDecisions = function(){
+        let board = gameBoard.getGameboard();
+        console.log(firstPlayerChoices);
+        for(i=0; i < board.length; i++){
+            if(firstPlayerChoices[i] = board[i]){
+               board[1] = "x";
+               console.log("called");
+            }
+        }
+        return console.log(board);
+    }
+}
+
 const playerFactory = (nick, marker, choices) => { 
     return {nick, marker, choices};
 }
 
 const firstPlayer = playerFactory('Gyromitte', "X", choices = [1, 4, 7]);
+const secondPlayer = playerFactory('CPU', "O", choices = [2, 5, 8]);
 
 console.log(firstPlayer.choices);
-gameBoard = new gameBoard(3, 2);
+console.log(secondPlayer.choices);
+
+gameBoard = new gameBoard();
 gameBoard.getGameboard();
+
+gameFlow = new gameFlow(firstPlayer.choices, secondPlayer.choices);
+gameFlow.showDecisions();
 
