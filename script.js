a83d540022f2ea014ef5bfa6fa968516f844541f
Gyromitte <gyromitte@gmail.com>
2021-10-20 15:42:57 -0500

    The logic of the  gameflow alghorithm has been  fixed

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const playerFactory = (nick, marker, choices) => { 
     return {nick, marker, choices};
 }
-const firstPlayer = playerFactory('Gyromitte', "X", choices = [1, 4, 7]);
+
+const firstPlayer = playerFactory('Gyromitte', "X", choices = [7, 4, 1]);
 const secondPlayer = playerFactory('CPU', "O", choices = [2, 5, 8]);
-
-
 
 function gameBoard(){
     let board = [1,2,3,4,5,6,7,8,9];
@@ -17,27 +16,16 @@ function gameBoard(){
     }
 }
 
-
 function gameFlow(firstPlayerChoices){
-
     this.showDecisions = function(){
         let newBoard = theGameBoard.getGameboard();
-        //TODO
-        let j = 0;
-        for(var i=0;  i < theGameBoard.getGameboard().length; i++){
-
-            //J es un contador aparte para recorrer firsPlayerChoices y solo aumentar cuando el numero coincida
-
-            console.log(i);
-            if(firstPlayerChoices[j] == theGameBoard.getGameboard()[i]){
-               console.log("indice: " + firstPlayerChoices[j]);
-               newBoard.splice(i, 1, "x");
-
-               j++;
-                console.log(firstPlayerChoices[j]);
-            }
+        //Iterate trough the player choices
+        for(var i=0;  i < firstPlayerChoices.length; i++){
+            if(theGameBoard.getGameboard().indexOf(firstPlayerChoices[i]) !== -1){
+                //The board is modified to show markers in the correct positions 
+                newBoard.splice(theGameBoard.getGameboard().indexOf(firstPlayerChoices[i]), 1, "x" );
+            }   
         }
-        console.log(theGameBoard.getGameboard());
         return console.log(newBoard);
     }
 }
