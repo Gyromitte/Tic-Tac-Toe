8946f61d96ce54f96ee812150e1c8eb319d245d2
Gyromitte <gyromitte@gmail.com>
2021-10-19 20:13:25 -0500

    The x characters can now be display in the board array

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
+const playerFactory = (nick, marker, choices) => { 
+    return {nick, marker, choices};
+}
+const firstPlayer = playerFactory('Gyromitte', "X", choices = [1, 4, 7]);
+const secondPlayer = playerFactory('CPU', "O", choices = [2, 5, 8]);
+
+
+
 function gameBoard(){
-    let board = ["1",2,3,4,5,6,7,8,9];
+    let board = [1,2,3,4,5,6,7,8,9];
 
     this.getGameboard = function(){
-        let fixBoard =  board[0] + board[1] + board[2] + "\n" +
-                        board[3] + board[4] + board[5] + "\n" +
-                        board[6] + board[7] + board[8];
-        console.log(fixBoard);
+        let fixBoard =  [board[0], + board[1], + board[2], + "\n" +
+                        board[3], + board[4], + board[5], + "\n" +
+                        board[6], + board[7], + board[8]];
         return fixBoard;
     }
 }
 
-function gameFlow(firstPlayerChoices, secondPlayerChoices){
+
+function gameFlow(firstPlayerChoices){
+
     this.showDecisions = function(){
-        let board = gameBoard.getGameboard();
-        console.log(firstPlayerChoices);
-        for(i=0; i < board.length; i++){
-            if(firstPlayerChoices[i] = board[i]){
-               board[1] = "x";
-               console.log("called");
+        let newBoard = theGameBoard.getGameboard();
+        //TODO
+        let j = 0;
+        for(var i=0;  i < theGameBoard.getGameboard().length; i++){
+
+            //J es un contador aparte para recorrer firsPlayerChoices y solo aumentar cuando el numero coincida
+
+            console.log(i);
+            if(firstPlayerChoices[j] == theGameBoard.getGameboard()[i]){
+               console.log("indice: " + firstPlayerChoices[j]);
+               newBoard.splice(i, 1, "x");
+
+               j++;
+                console.log(firstPlayerChoices[j]);
             }
         }
-        return console.log(board);
+        console.log(theGameBoard.getGameboard());
+        return console.log(newBoard);
     }
 }
 
-const playerFactory = (nick, marker, choices) => { 
-    return {nick, marker, choices};
-}
 
-const firstPlayer = playerFactory('Gyromitte', "X", choices = [1, 4, 7]);
-const secondPlayer = playerFactory('CPU', "O", choices = [2, 5, 8]);
+theGameBoard = new gameBoard();
+theGameBoard.getGameboard();
 
-console.log(firstPlayer.choices);
-console.log(secondPlayer.choices);
-
-gameBoard = new gameBoard();
-gameBoard.getGameboard();
-
-gameFlow = new gameFlow(firstPlayer.choices, secondPlayer.choices);
-gameFlow.showDecisions();
+theGameFlow = new gameFlow(firstPlayer.choices);
+theGameFlow.showDecisions();
 
